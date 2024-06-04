adf06d2401c9537d830426d27da6b4043031338c
arsalanendor <aahmed@endor.ai>
2024-06-04 12:10:14 -0500

    testting new pr vuln

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -2,6 +2,7 @@ package org.example.app;
 
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -13,12 +14,13 @@ public class App {
             DiskFileItem fileItem = new DiskFileItem("file", "text/plain", false, "test.txt", 1000000, repository);
 
             // Vulnerable method: write
-            File file = new File("vulnerable.txt");
-            OutputStream os = fileItem.getOutputStream();
-            os.write("This is a test".getBytes());
-            os.close();
-            
-            System.out.println("File created: " + file.getAbsolutePath());
+            String fileName = "../../vulnerable.txt"; // Directory traversal vulnerability
+            File file = new File(repository, fileName);
+            try (OutputStream os = new FileOutputStream(file)) {
+                os.write("This is a test".getBytes());
+            }
+
+            System.out.println("File created: " + file.getCanonicalPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
