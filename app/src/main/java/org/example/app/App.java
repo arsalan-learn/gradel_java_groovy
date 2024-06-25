b703d20f5a48a03ae5c3f0c32b91993fb4bd8bde
arsalanendor <aahmed@endor.ai>
2024-06-25 10:43:40 -0500

    added vulns again

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -1,10 +1,26 @@
 package org.example.app;
 
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class App {
     public static void main(String[] args) {
-    
-            System.out.println("some random text ");
-        
+        // Simulating the use of a vulnerable function in DiskFileItem
+        try {
+            File repository = new File(System.getProperty("java.io.tmpdir"));
+            DiskFileItem fileItem = new DiskFileItem("file", "text/plain", false, "test.txt", 1000000, repository);
+
+            // Vulnerable method: write
+            File file = new File("vulnerable.txt");
+            OutputStream os = fileItem.getOutputStream();
+            os.write("This is a test".getBytes());
+            os.close();
+            
+            System.out.println("File created: " + file.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
