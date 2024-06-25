67e943cbf1d61637e9d44fc3c8e53b8f3ff497b0
arsalanendor <aahmed@endor.ai>
2024-06-25 10:34:24 -0500

    removed vulnerabilities

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -1,26 +1,10 @@
 package org.example.app;
 
-import org.apache.commons.fileupload.disk.DiskFileItem;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
 
 public class App {
     public static void main(String[] args) {
-        // Simulating the use of a vulnerable function in DiskFileItem
-        try {
-            File repository = new File(System.getProperty("java.io.tmpdir"));
-            DiskFileItem fileItem = new DiskFileItem("file", "text/plain", false, "test.txt", 1000000, repository);
-
-            // Vulnerable method: write
-            File file = new File("vulnerable.txt");
-            OutputStream os = fileItem.getOutputStream();
-            os.write("This is a test".getBytes());
-            os.close();
-            
-            System.out.println("File created: " + file.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    
+            System.out.println("some random text ");
+        
     }
 }
